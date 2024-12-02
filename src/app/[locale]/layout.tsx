6704e2b350e5f7902559c7421a1b06ce4793d30c
Vladyslav Rybak <35425030+vrybakk@users.noreply.github.com>
2024-12-02 13:38:46 +0100

    Initialize feat/next-15

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,36 @@
+import {routing} from '@/src/i18n/routing';
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {ReactNode} from 'react';
+
+type Props = {
+  children: ReactNode;
+  params: Promise<{locale: string}>;
+};
+
+export default async function LocaleLayout({children, params}: Props) {
+  const {locale} = await params;
+
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <head>
+        <title>next-intl-bug-repro-app-router</title>
+      </head>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
